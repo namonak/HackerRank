eda4825b434ca5212b25e6c2eefec70580888e55
namonak <namonak@gmail.com>
2019-07-31 01:58:40 +0900

    HackerRank  - Functions in C : https://www.hackerrank.com/challenges/functions-in-c/problem  - Pointers in C : https://www.hackerrank.com/challenges/pointer-in-c/problem

---
+++ C/PointersInC/pointers_in_c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    Complete this function    
+        int _a = (*a) + (*b);
+    int _b = (*a) > (*b) ? (*a) - (*b) : (*b) - (*a);
+
+    (*a) = _a;
+    (*b) = _b;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
