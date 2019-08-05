d7be8d51aba9731d217baadb93a7deb23335489c
namonak <namonak@gmail.com>
2019-08-05 23:45:24 +0900

    HackerRank  - 1D Arrays in C :https://www.hackerrank.com/challenges/1d-arrays-in-c/problem

---
+++ C/1DArraysinC/1d_arrays_in_c.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {    
+    int count = 0;
+    int *arr = NULL;
+    int sum = 0;    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    scanf("%d", &count);
+    if (count <= 0 || count > 1000) {
+        return -1;
+    }
+
+    arr = (int*)malloc(sizeof(int) * count);
+    if (arr == NULL) {
+        return -1;
+    }
+
+    for (int i = 0; i < count; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] <= 0 || arr[i] > 1000) {
+            return -1;
+        }
+        sum += arr[i];
+    }
+
+    printf("%d\n", sum);
+
+    free(arr);
+
+    return 0;
+}
