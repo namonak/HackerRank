da53a1eab32df15106ba31d329146c394534cf1a
namonak <namonak@gmail.com>
2019-08-13 00:02:34 +0900

    Unused package handling
      - Reference : http://pyrasis.com/book/GoForTheReallyImpatient/Unit07/02

--- 30_Days_of_Code/Day1-Data_Types.go
+++ 30_Days_of_Code/Day1-Data_Types.go
@@ -4,11 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	_ "strconv"
 )
 
 func main() {
-	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
+	//var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
 
 	var i uint64 = 4
 	var d float64 = 4.0
