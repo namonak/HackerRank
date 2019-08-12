c1f507e88e75301d8308cdd9e02c26bc2f85dfe0
namonak <namonak@gmail.com>
2019-08-12 23:20:31 +0900

    Applied Golang formatting
    - go fmt ./...

--- 30_Days_of_Code/Day1-Data_Types.go
+++ 30_Days_of_Code/Day1-Data_Types.go
@@ -1,40 +1,39 @@
 package main
 
-
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-  	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
-  
-    var i uint64 = 4
-    var d float64 = 4.0
-    var s string = "HackerRank "
+	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
 
-    scanner := bufio.NewScanner(os.Stdin)
-    
-    // Declare second integer, double, and String variables.
-    var _i uint64
-    var _d float64
-    var _s string
-    
-    // Read and save an integer, double, and String to your variables.
-    fmt.Scan(&_i)
-    fmt.Scan(&_d)
-    scanner.Scan()
-    _s = scanner.Text()
+	var i uint64 = 4
+	var d float64 = 4.0
+	var s string = "HackerRank "
 
-    // Print the sum of both integer variables on a new line.
-    fmt.Println(i + _i)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    // Print the sum of the double variables on a new line.
-    fmt.Printf("%.1f\n", d + _d)
+	// Declare second integer, double, and String variables.
+	var i2 uint64
+	var d2 float64
+	var s2 string
 
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
-    fmt.Printf("%s%s", s, _s)
+	// Read and save an integer, double, and String to your variables.
+	fmt.Scan(&i2)
+	fmt.Scan(&d2)
+	scanner.Scan()
+	s2 = scanner.Text()
+
+	// Print the sum of both integer variables on a new line.
+	fmt.Println(i + i2)
+
+	// Print the sum of the double variables on a new line.
+	fmt.Printf("%.1f\n", d+d2)
+
+	// Concatenate and print the String variables on a new line
+	// The 's' variable above should be printed first.
+	fmt.Printf("%s%s", s, s2)
 }
--- 30_Days_of_Code/Day2-Operators.go
+++ 30_Days_of_Code/Day2-Operators.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
-    "strconv"
-    "strings"
-    "math"
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
-// Complete the solve function below.
-func solve(meal_cost float64, tip_percent int32, tax_percent int32) {    
-    var tip float64 = meal_cost * (float64(tip_percent) / 100)
-    var tax float64 = meal_cost * (float64(tax_percent) / 100)
-    var total_cost float64
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-    total_cost = meal_cost + tip + tax
+	meal_cost, err := strconv.ParseFloat(readLine(reader), 64)
+	checkError(err)
 
-    fmt.Println(math.Round(total_cost))
+	tip_percentTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	tip_percent := int32(tip_percentTemp)
+
+	tax_percentTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	tax_percent := int32(tax_percentTemp)
+
+	solve(meal_cost, tip_percent, tax_percent)
 }
 
-func main() {
-    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+// Complete the solve function below.
+func solve(meal_cost float64, tip_percent int32, tax_percent int32) {
+	var tip float64 = meal_cost * (float64(tip_percent) / 100)
+	var tax float64 = meal_cost * (float64(tax_percent) / 100)
+	var total_cost float64
 
-    meal_cost, err := strconv.ParseFloat(readLine(reader), 64)
-    checkError(err)
+	total_cost = meal_cost + tip + tax
 
-    tip_percentTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-    checkError(err)
-    tip_percent := int32(tip_percentTemp)
-
-    tax_percentTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-    checkError(err)
-    tax_percent := int32(tax_percentTemp)
-
-    solve(meal_cost, tip_percent, tax_percent)
+	fmt.Println(math.Round(total_cost))
 }
 
 func readLine(reader *bufio.Reader) string {
-    str, _, err := reader.ReadLine()
-    if err == io.EOF {
-        return ""
-    }
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
 
-    return strings.TrimRight(string(str), "\r\n")
+	return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
