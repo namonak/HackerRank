0649b56fcac3ead04c08609c222033f2803c4185
namonak <namonak@gmail.com>
2019-08-15 23:45:21 +0900

    30 Days of Code / Day 0
    - Day 0, Hello, World. : https://www.hackerrank.com/challenges/30-hello-world/problem

---
+++ 30_Days_of_Code/Day0-Hello_World.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	in := bufio.NewReader(os.Stdin)
+	line, err := in.ReadString('\n')
+	if err != nil {
+		// exception
+	}
+	fmt.Println("Hello, World.")
+	fmt.Println(line)
+}
