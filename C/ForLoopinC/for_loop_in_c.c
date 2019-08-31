2c91f56b32dab405cf4427c889cbc3abd68de3e4
namonak <namonak@gmail.com>
2019-08-31 22:53:23 +0900

    HackerRank  - For Loop in C : https://www.hackerrank.com/challenges/for-loop-in-c/problem

---
+++ C/ForLoopinC/for_loop_in_c.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void convertDigitToEnglish(int digit) {
+    char numberArr[10][15] = {"zero, invalid", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    printf("%s\n", numberArr[digit]);
+}
+
+void isEvenOdd(int digit) {
+    if (digit % 2 == 0) {
+        printf("even\n");
+    } else {
+        printf("odd\n");
+    }
+}
+
+int main() 
+{
+    int a, b;    
+
+    scanf("%d\n%d", &a, &b);
+    // Complete the code.
+
+    if (a == 0) {
+        a = 1;
+    }
+
+    if (b >= 10) {
+        b = 9;
+    }
+
+    for (int i = a; i <= b; i++) {
+        convertDigitToEnglish(i);
+    }
+
+    for (int i = a; i <= b; i++) {
+        isEvenOdd(i);
+    }
+
+    return 0;
+}
+
