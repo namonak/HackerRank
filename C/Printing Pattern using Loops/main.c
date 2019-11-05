4f0b1945a902a0d8101cc7e1030ae39d36127094
namonak <namonak@gmail.com>
2019-11-06 01:34:57 +0900

    About half finished. But it could be another start...

--- C/Printing Pattern using Loops/main.c
+++ C/Printing Pattern using Loops/main.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 
-int main() 
+void printPattern(int a, int b) {
+    int loop = a;
+
+    for (int i = 1; i <= loop; i++) {
+        printf("%d ", a);
+        if (a != b) {
+            a--;
+        }
+    }
+
+    for (int j = 1; j <= loop - 1; j++) {
+        if (j >= b) {
+            a++;
+        }
+        printf("%d ", a);
+
+    }
+    printf("\n");
+}
+
+int main()
 {
     int n;
-    int loop;
     scanf("%d", &n);
   	// Complete the code to print the pattern.
 
-    loop = (n * 2) - 1;
-
-    for (int i = 0; i < loop; i++) {
-        printf("%d ", n);
+    for (int i = n; i >= 1; i--) {
+        printPattern(n, i);
     }
-    printf("\n");
 
     return 0;
 }
