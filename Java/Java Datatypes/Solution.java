9832db663b43e584369ff15245963e9a8a4b31f9
namonak <namonak@gmail.com>
2019-11-18 23:48:24 +0900

    Java Datatypes : https://www.hackerrank.com/challenges/java-datatypes/problem

--- Java/Java Datatypes/Solution.java
+++ Java/Java Datatypes/Solution.java
@@ -1,35 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public static void main(String []argh) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        for (int i = 0; i < n; i++) {
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
             try {
                 long x = sc.nextLong();
-
                 System.out.println(x+" can be fitted in:");
 
-                if (x >= -128 && x <= 127) {
+                // Reference : https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+                if (x >= (byte)(-Math.pow(2, 7)) && x <= (byte)(Math.pow(2, 7) - 1)) {
                     System.out.println("* byte");
                 }
-                //Complete the codes 
-                if(x >= -32768 && x <= 32767) {
+                if (x >= (short)(-Math.pow(2, 15)) && x <= (short)(Math.pow(2, 15) - 1)) {
                     System.out.println("* short");
                 }
-                if (x >= -2147483648 && x <= 2147483647) {
+                if (x >= (int)(-Math.pow(2, 31)) && x <= (int)(Math.pow(2, 31) - 1)) {
                     System.out.println("* int");
                 }
-                if (x >= -9223372036854775808l && x <= 9223372036854775807l) {
-                    System.out.print("* long");
+                if (x >= (long)(-Math.pow(2, 63)) && x <= (long)(Math.pow(2, 63) - 1)) {
+                    System.out.println("* long");
                 }
             } catch(Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
+                System.out.println(sc.next()+" can't be fitted anywhere.");
             }
         }
-
-        sc.close();
     }
 }
