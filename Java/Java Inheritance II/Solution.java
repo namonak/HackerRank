c185727ede6448c8259826c8a1077ef332dc8336
namonak <namonak@gmail.com>
2019-11-29 12:27:33 +0900

    Java Inheritance II : https://www.hackerrank.com/challenges/java-inheritance-2/problem

--- Java/Java Inheritance II/Solution.java
+++ Java/Java Inheritance II/Solution.java
@@ -5,6 +5,14 @@ import java.math.*;
 import java.util.regex.*;
 
 //Write your code here
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+}
 
 public class Solution{
     public static void main(String []args){
