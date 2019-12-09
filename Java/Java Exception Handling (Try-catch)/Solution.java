0b24e87c2fdb5c7cbbbedc276d939231a3747a87
namonak <namonak@gmail.com>
2019-12-09 23:45:14 +0900

    Java Exception Handling (Try-catch) : https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem

--- Java/Java Exception Handling (Try-catch)/Solution.java
+++ Java/Java Exception Handling (Try-catch)/Solution.java
@@ -7,5 +7,14 @@ import java.util.regex.*;
 public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner cin = new Scanner(System.in);
+		try {
+			int x = cin.nextInt(), y = cin.nextInt();
+			System.out.println(x/y);
+		} catch (InputMismatchException e) {
+			System.out.println(e.getClass().toString().substring(6));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
     }
 }
