de5d5d0985b7eb6f666c6b5622a0df419f9ad907
namonak <namonak@gmail.com>
2020-09-10 15:12:22 +0900

    Calculate the Nth term : https://www.hackerrank.com/challenges/recursion-in-c/problem

--- C/Functions/Calculate the Nth term/main.c
+++ C/Functions/Calculate the Nth term/main.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 //Complete the following function.
 
-int find_nth_term(int n, int a, int b, int c)
-{
+int find_nth_term(int n, int a, int b, int c) {
     //Write your code here.
-    if(n == 1) {
-        return a;
-    } else if(n == 2) {
-        return b;
-    } else if(n == 3) {
-        return c;
-    } else {
-        return (n - 1) + (n - 2) + (n - 3);
-    }        
+    if (n == 1) return a;
+    if (n == 2) return b;
+    if (n == 3) return c;
+
+    return find_nth_term(n-3, a, b, c) + find_nth_term(n-2, a, b, c) + find_nth_term(n-1, a, b, c); 
 }
 
-int main()
-{
+int main() {
     int n, a, b, c;
-  
+
     scanf("%d %d %d %d", &n, &a, &b, &c);
     int ans = find_nth_term(n, a, b, c);
- 
+
     printf("%d", ans);
-    
     return 0;
 }
