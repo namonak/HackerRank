67eb0465432b615c941740fc4f139f1ba58e6cda
namonak <namonak@gmail.com>
2020-09-10 15:16:53 +0900

    Calculate the Nth term : https://www.hackerrank.com/challenges/recursion-in-c/problem

--- C/Functions/Calculate the Nth term/main.c
+++ C/Functions/Calculate the Nth term/main.c
@@ -10,7 +10,7 @@ int find_nth_term(int n, int a, int b, int c) {
     if (n == 2) return b;
     if (n == 3) return c;
 
-    return find_nth_term(n-3, a, b, c) + find_nth_term(n-2, a, b, c) + find_nth_term(n-1, a, b, c); 
+    return find_nth_term(n-1, a, b, c) + find_nth_term(n-2, a, b, c) + find_nth_term(n-3, a, b, c);
 }
 
 int main() {
