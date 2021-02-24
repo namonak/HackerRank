263b0878b15f55b97e79c49e1e87a6237d64a085
namonak <namonak@gmail.com>
2021-02-24 23:14:44 +0900

    Separate the Numbers : https://www.hackerrank.com/challenges/separate-the-numbers/problem

---
+++ Algorithms/Strings/Separate the Numbers/main.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the separateNumbers function below.
+fun separateNumbers(s: String): Unit {
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val q = scan.nextLine().trim().toInt()
+
+    for (qItr in 1..q) {
+        val s = scan.nextLine()
+
+        separateNumbers(s)
+    }
+}
