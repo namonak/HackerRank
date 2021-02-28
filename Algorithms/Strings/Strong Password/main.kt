7aa534ee92be6e43358600f54339ddc0db5111b2
namonak <namonak@gmail.com>
2021-02-28 22:39:49 +0900

    Strong Password : https://www.hackerrank.com/challenges/strong-password/problem

---
+++ Algorithms/Strings/Strong Password/main.kt
@@ -0,0 +1,37 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the minimumNumber function below.
+fun minimumNumber(n: Int, password: String): Int {
+    // Return the minimum number of characters to make the password strong
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val password = scan.nextLine()
+
+    val answer = minimumNumber(n, password)
+
+    println(answer)
+}
+
