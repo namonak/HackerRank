dc5a00d06947f9576ba5694788873df18b889d55
namonak <namonak@gmail.com>
2021-03-08 23:19:22 +0900

    Big Sorting : https://www.hackerrank.com/challenges/big-sorting/problem

---
+++ Algorithms/Sorting/Big Sorting/main.kt
@@ -0,0 +1,41 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the bigSorting function below.
+fun bigSorting(unsorted: Array<String>): Array<String> {
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val unsorted = Array<String>(n, { "" })
+    for (i in 0 until n) {
+        val unsortedItem = scan.nextLine()
+        unsorted[i] = unsortedItem
+    }
+
+    val result = bigSorting(unsorted)
+
+    println(result.joinToString("\n"))
+}
+
