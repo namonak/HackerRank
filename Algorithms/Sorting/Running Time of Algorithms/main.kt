209e4619ed42886e056f277334e3158d7c5ef5ca
namonak <namonak@gmail.com>
2021-03-10 22:22:07 +0900

    Running Time of Algorithms : https://www.hackerrank.com/challenges/runningtime/problem

---
+++ Algorithms/Sorting/Running Time of Algorithms/main.kt
@@ -0,0 +1,37 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the runningTime function below.
+fun runningTime(arr: Array<Int>): Int {
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = runningTime(arr)
+
+    println(result)
+}
+
