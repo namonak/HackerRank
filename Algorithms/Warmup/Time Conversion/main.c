f394a864c301b999f17fa9e3949f4eaa894d264d
namonak <namonak@gmail.com>
2021-10-13 00:38:29 +0900

    Time Conversion : https://www.hackerrank.com/challenges/time-conversion/problem

---
+++ Algorithms/Warmup/Time Conversion/main.c
@@ -0,0 +1,141 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* timeConversion(char* s) {
+    char *ret = NULL;
+    char hour[3] = {0, };
+    char ampm[3] = {0, };
+    int len = strlen(s);
+    int tmp = 0;
+    char temp[10];
+
+    ret = (char*)malloc(len + 1);
+
+    snprintf(hour, 3, "%s", s);
+    tmp = atoi(hour);
+
+    ampm[0] = s[len - 2];
+    ampm[1] = s[len - 1];
+    ampm[2] = '\0';
+
+    if (strcmp(ampm, "AM") == 0) {
+        if (tmp == 12) {
+            strncpy(temp, s + 2, len - 4);
+            sprintf(ret, "%s%s", "00", temp);
+        } else {
+            strncpy(ret, s, len - 2);
+        }
+    } else {
+        if (tmp == 12) {
+            strncpy(ret, s, len - 2);
+        } else {
+            strncpy(temp, s + 2, len - 4);
+            sprintf(ret, "%d%s", tmp + 12, temp);
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
